8dd83db02f91b6dbb24faabb4637835b372f164b
Suyi <thonatos.yang@gmail.com>
2024-01-03 21:02:29 +0800

    chore: update config for prettier

---
+++ .prettierrc.js
@@ -0,0 +1,16 @@
+// Documentation for this file: https://prettier.io/en/configuration.html
+
+module.exports = {
+  trailingComma: 'none',
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+
+  // We use a larger print width because Prettier's word-wrapping seems to be tuned
+  // for plain JavaScript without type annotations
+  printWidth: 110,
+
+  // Use .gitattributes to manage newlines
+  endOfLine: 'lf'
+  // plugins: ['./common/autoinstallers/rush-prettier/node_modules/prettier-plugin-packagejson']
+};
