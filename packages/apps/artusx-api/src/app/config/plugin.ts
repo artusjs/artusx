eab683a4ceda7d130f41a0fce35368d3b2a7298b
Suyi <thonatos.yang@gmail.com>
2024-01-04 13:51:06 +0800

    feat: move redis/sequelize out from framework

--- packages/apps/artusx-api/src/app/config/plugin.ts
+++ packages/apps/artusx-api/src/app/config/plugin.ts
@@ -1,1 +1,11 @@
-export default {};
+export default {
+  redis: {
+    enable: true,
+    package: '@artusx/plugin-redis'
+  },
+
+  sequelize: {
+    enable: true,
+    package: '@artusx/plugin-sequelize'
+  }
+};
--- packages/libs/framework/src/config/plugin.ts
+++ packages/libs/framework/src/config/plugin.ts
@@ -1,14 +1,4 @@
 export default {
-  redis: {
-    enable: true,
-    package: '@artusx/plugin-redis'
-  },
-
-  sequelize: {
-    enable: true,
-    package: '@artusx/plugin-sequelize'
-  },
-
   'application-http': {
     enable: true,
     package: '@artusx/plugin-application-http'
