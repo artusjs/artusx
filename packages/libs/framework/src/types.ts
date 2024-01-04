9bbae35bcad239d7ed62ea6a934e7833de5b58d1
Suyi <thonatos.yang@gmail.com>
2024-01-04 11:36:40 +0800

    feat: change dist dir to lib for libs

---
+++ packages/libs/framework/src/types.ts
@@ -0,0 +1,4 @@
+export { default as IRedisClient } from '@artusx/plugin-redis/lib/client';
+export { default as ISequelizeClient } from '@artusx/plugin-sequelize/lib/client';
+
+export * from '@artusx/plugin-application-http';
