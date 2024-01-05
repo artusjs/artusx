113da91acf2e9fe834157a467f0d8b3af4d665ab
Suyi <thonatos.yang@gmail.com>
2024-01-05 15:03:23 +0800

    feat: add bodyparser to core

--- packages/libs/core/src/app.ts
+++ packages/libs/core/src/app.ts
@@ -8,6 +8,7 @@ import {
 } from '@artus/core';
 
 import cors from '@koa/cors';
+import { bodyParser } from '@koa/bodyparser';
 import KoaApplication from '@artusx/plugin-application-http/lib/koa/application';
 
 @LifecycleHookUnit()
@@ -21,6 +22,15 @@ export default class ArtusXCoreLifecycle implements ApplicationLifecycle {
 
   @LifecycleHook()
   async didLoad() {
+    this.koa.use(
+      bodyParser({
+        encoding: 'utf-8',
+        onError(_err, ctx) {
+          ctx.throw(422, 'body parse error');
+        }
+      })
+    );
+
     this.koa.use(
       cors({
         credentials: true,
