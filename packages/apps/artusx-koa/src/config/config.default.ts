163888a66e0f95749f4793dec48f3883a72397e1
Suyi <thonatos.yang@gmail.com>
2024-02-02 16:41:06 +0800

    feat(artusx-koa): update config for xprofiler and xtransit

--- packages/apps/artusx-koa/src/config/config.default.ts
+++ packages/apps/artusx-koa/src/config/config.default.ts
@@ -1,6 +1,7 @@
 import os from 'os';
 import path from 'path';
 import fs from 'fs-extra';
+import { LoggerOptions, LoggerLevel } from '@artus/core';
 
 const tmpDir = os.tmpdir();
 const rootDir = path.resolve(__dirname, '../..');
@@ -11,13 +12,25 @@ export default () => {
   fs.ensureDirSync(logsDir);
   fs.ensureDirSync(xprofilerLogDir);
 
+  const logger: LoggerOptions = {
+    level: LoggerLevel.DEBUG,
+  };
+
   return {
+    logger,
+
+    xprofiler: {
+      log_level: 0,
+      enable_http_profiling: true,
+    },
+
     xtransit: {
       server: 'ws://127.0.0.1:9190',
       appId: '1',
       appSecret: '88115b3f0881348fe4a8935b103c0a74',
 
       logDir: xprofilerLogDir,
+      logInterval: 60,
 
       errors: [`${logsDir}/logs/common-error.log`, `${logsDir}/logs/stderr.log`],
       packages: [`${rootDir}/package.json`],
