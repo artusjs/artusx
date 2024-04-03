0d0b1221b4e9bc5408b39dc9917ddb810e9509d3
Suyi <thonatos.yang@gmail.com>
2024-04-04 01:48:15 +0800

    feat(@artusx/plugin-log4js): support silent start-info

--- packages/plugins/log4js/src/lifecycle.ts
+++ packages/plugins/log4js/src/lifecycle.ts
@@ -21,8 +21,12 @@ export default class Log4jsLifecycle implements ApplicationLifecycle {
 
   @LifecycleHook()
   async willReady() {
+    const silent = this.app.config.silent?.log4js || false;
     const config = this.app.config.log4js || defaultLog4jsConfig;
-    this.logger.info('[log4js] starting log4js with appenders: \n %o', config.appenders);
+
+    if (!silent) {
+      this.logger.info('[log4js] starting log4js with appenders: \n %o', config.appenders);
+    }
 
     const client = this.app.container.get(ArtusXInjectEnum.Client) as Client;
     await client.init(config);
