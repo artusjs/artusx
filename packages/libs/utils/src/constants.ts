da811e7b270bd9508d0d63d6ea86733498cce807
Suyi <thonatos.yang@gmail.com>
2024-10-08 12:06:42 +0000

    feat(plugin-clickhouse): update clickhouse types

--- packages/libs/utils/src/constants.ts
+++ packages/libs/utils/src/constants.ts
@@ -16,7 +16,7 @@ export enum PluginInjectEnum {
   Redis = 'ARTUSX_REDIS',
   Sequelize = 'ARTUSX_SEQUELIZE',
   Prometheus = 'ARTUSX_PROMETHEUS',
-  Clickhouse = 'ARTUSX_CLICKHOUSE',
+  ClickHouse = 'ARTUSX_CLICKHOUSE',
 
   GRPC = 'ARTUSX_GRPC',
   PPTR = 'ARTUSX_PPTR',
